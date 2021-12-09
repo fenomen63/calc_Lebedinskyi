0e5d165f6f9778256f330b823b49e3db3bca34b0
student <student@05-15>
2021-12-09 20:37:40 +0200

    функции меньше больше

--- calculator/calculator/calculator.cpp
+++ calculator/calculator/calculator.cpp
@@ -12,6 +12,28 @@ int Umnoj (int a, int b) {
 int Delenie (int a, int b) {
     return a / b;
 }
+bool Menshe(int a, int b) {
+    if (a < b) {
+        return a;
+    }
+    else if (b < a) {
+        return b;
+    }
+    else {
+        return a = b;
+    }
+}
+bool Bolshe(int a, int b) {
+    if (a < b) {
+        return b;
+    }
+    else if (b < a) {
+        return a;
+    }
+    else {
+        return a = b;
+    }
+}
 int main()
 {
     int a, b;
